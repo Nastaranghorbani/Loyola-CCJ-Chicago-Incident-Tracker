029f90f2aaa01cb53359c50708931b0443a431da
Nastaranghorbani <114106255+Nastaranghorbani@users.noreply.github.com>
2024-05-16 16:02:46 -0500

    Update Interrupted Time Series Analysis.py

--- Interrupted Time Series Analysis.py
+++ Interrupted Time Series Analysis.py
@@ -28,20 +28,27 @@ def causal_effect(df, treatment_iso_week_str):
 if __name__ == "__main__":
     # Import data
     df = pd.read_csv('https://raw.githubusercontent.com/Nastaranghorbani/Loyola-CCJ-Chicago-Incident-Tracker/main/data/inc_data_selected.csv')
+
     # Convert the 'date' column to a pandas datetime object
     df['date'] = pd.to_datetime(df['date'])
+
     # Extract the ISO week number and year from the 'date' column
     df['week'] = df['date'].dt.isocalendar().week
     df['year'] = df['date'].dt.year
-    # Group the data by 'year' and 'week'
-    week_sum = df.groupby(['year', 'week']).sum().reset_index()
+
+    # Group the data by 'year' and 'week', and sum only numeric columns
+    numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()  # selecting only numeric columns
+    week_sum = df.groupby(['year', 'week'])[numeric_cols].sum().reset_index()
+
     # Create 'ISO_Week' column
     week_sum['ISO_Week'] = week_sum['year'].astype(str) + '-' + week_sum['week'].apply(lambda x: f'{x:02}')
+
     # Treatment date and convert to ISO week string
     treatment_date = pd.to_datetime("2023-09-18")
     treatment_iso_year = treatment_date.isocalendar().year
     treatment_iso_week = treatment_date.isocalendar().week
     treatment_iso_week_str = f"{treatment_iso_year}-{treatment_iso_week:02}"
+
     # Assign time and calculate 'y'
     week_sum = week_sum.assign(time=np.arange(len(week_sum)))
     β0 = 0
@@ -49,10 +56,12 @@ if __name__ == "__main__":
     N = len(week_sum)
     week_sum['y'] = β0 + β1 * week_sum['time'] + causal_effect(week_sum, treatment_iso_week_str) + norm(0, 0.5).rvs(N)
     week_sum.reset_index(drop=True, inplace=True)
+
     # Split the data into pre- and post-intervention
     mask = week_sum['ISO_Week'] < treatment_iso_week_str
     pre = week_sum[mask]
     post = week_sum[~mask]
+
     # Plotting the intervention analysis
     fig, ax = plt.subplots()
     pre['y'].plot(ax=ax, label="Pre-Intervention")
